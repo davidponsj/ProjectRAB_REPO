14eada2239df69b6e6270ff9ed01569e9b1ea976
sgarcval-max <sgarcval@gmail.com>
2025-11-11 22:54:11 +0100

    Update_CountDown

--- ProjectRAB_Project/Assets/RAB_Root/Scripts/Countdown.cs
+++ ProjectRAB_Project/Assets/RAB_Root/Scripts/Countdown.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
-using Unity.VisualScripting.Antlr3.Runtime.Tree;
 
 public class Countdown : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
     [SerializeField] float remainingTime;
 
-    // Update is called once per frame
+    [SerializeField] string sceneName;      // Escena a cargar
+    [SerializeField] string checkpointName; // Nombre del GameObject checkpoint
+
+    bool hasEnded = false;
+
     void Update()
     {
         if (remainingTime > 0)
@@ -15,21 +19,34 @@ public class Countdown : MonoBehaviour
             remainingTime -= Time.deltaTime;
 
             if (remainingTime < 5)
-            {
                 timerText.color = Color.red;
-            }
         }
-
-        else if (remainingTime < 0)
+        else if (!hasEnded)
         {
             remainingTime = 0;
-           
+            hasEnded = true;
+
+            SceneManager.sceneLoaded += OnSceneLoaded; // Suscribirse al evento
+            SceneManager.LoadScene(sceneName);         // Cargar escena
         }
-        
-        
+
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
-        
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Encontrar jugador y checkpoint
+        GameObject player = GameObject.FindWithTag("Player");
+        GameObject checkpoint = GameObject.Find(checkpointName);
+
+        if (player != null && checkpoint != null)
+        {
+            player.transform.position = checkpoint.transform.position;
+        }
+
+        SceneManager.sceneLoaded -= OnSceneLoaded; // Desuscribirse
     }
 }
+
