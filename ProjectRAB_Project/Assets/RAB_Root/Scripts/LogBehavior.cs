f11cb4e190d2c752c3f6e3d31f86dde3123e8ce3
MrHugo87 <hugoalemangarcia@hotmail.com>
2025-11-12 01:05:56 +0100

    LVL_1_COMPLETO+Troncos_desaparecen

--- ProjectRAB_Project/Assets/RAB_Root/Scripts/LogBehavior.cs
+++ ProjectRAB_Project/Assets/RAB_Root/Scripts/LogBehavior.cs
@@ -2,11 +2,20 @@ using UnityEngine;
 public class LogBehavior : MonoBehaviour
 {                   // ESTE SCRIPT SE LE AÑADE AL PREFAB DEL TRONCO
     public float minYToDestroy = 1f;       // Altura mínima antes de desaparecer
+    public string playerTag = "Player";
 
     void Update()
     {
         if (transform.position.y < minYToDestroy)
         {
+            Destroy(gameObject);
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag(playerTag))
+        {
+                    // Destruir el tronco
             Destroy(gameObject);
         }
     }
